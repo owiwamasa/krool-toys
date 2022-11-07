4be06fab57e3470dce09bde7855c443ca15763d0
Owen Iwamasa <80862287+owiwamasa@users.noreply.github.com>
2022-11-06 20:51:25 -0500

    setup router

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
+import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 
 const RouteProvider = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Box>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Box>
   );
 };
 export default RouteProvider;
