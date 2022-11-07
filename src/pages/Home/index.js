71361d3b87285fafd6336b3de38e379298d16b10
Owen Iwamasa <80862287+owiwamasa@users.noreply.github.com>
2022-11-06 22:16:11 -0500

    added instagram embed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import axios from "axios";
+import { InstagramEmbed } from "react-social-media-embed";
 
 const Home = () => {
   const [projects, setProjects] = useState([]);
@@ -20,6 +21,7 @@ const Home = () => {
     // Print errors if any
     return <div>An error occured: {error.message}</div>;
   }
+  console.log(projects);
 
   return (
     <Box>
@@ -30,6 +32,15 @@ const Home = () => {
             src={`http://localhost:1337${project.attributes.featuredMedia.data.attributes.url}`}
             alt="featured"
           />
+          {project.attributes.instagram && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <InstagramEmbed
+                url={project.attributes.instagram}
+                width={328}
+                captioned
+              />
+            </div>
+          )}
         </Box>
       ))}
     </Box>
